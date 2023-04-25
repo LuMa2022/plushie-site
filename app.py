1226f4e7e4d320a1b0544f45bbceeb775fb2b34e
LuMa2022 <104971308+LuMa2022@users.noreply.github.com>
2023-04-25 13:25:45 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 #import flask and create app var
 app = Flask(__name__)
 
+#create list of items to sell
+PRODUCTS = [
+    {
+      'id': 1, 
+      'item': 'Stickers',
+      'description': 'Easter',
+      'cost': '£1'
+    },
+    {
+      'id': 2, 
+      'item': 'Plushie',
+      'description': 'Octopus',
+      'cost': '£5'
+    },
+    {
+      'id': 3, 
+      'item': 'Badge',
+      'description': 'Coding',
+    }
+]
+
 #what route is required/registered for app
 @app.route("/")
 def helloWorld():
-  return "Hello World"
-
-
-
+  return render_template('home.html', 
+                          products=PRODUCTS,
+                          company_name = 'Plush'
+                        )
+  
+#second page - as a json'd structure
+@app.route("/api/products")
+def list_products():
+  return jsonify(PRODUCTS)
 
 
 #this IF will check route and execute prog
